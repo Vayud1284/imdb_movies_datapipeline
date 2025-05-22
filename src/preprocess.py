01c78f888a04ae8e42638ee1502c4bd276574ea5
Vayud1284 <pvayud@gmail.com>
2025-05-22 21:19:52 +0530

    Adding Preprocess Data

---
+++ src/preprocess.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def process_data(data:pd.DataFrame)->pd.DataFrame:
+
+    data.columns=[col.replace("(min)",'') for col in data.columns]
+
+    #converting columns into lower case
+    data.columns=data.columns.str.strip().str.lower().str.replace(" ","_")
+
+    #drop the empty title and poster
+    data=data.dropna(subset=["poster","title"])
+    data.drop_duplicates(inplace=True)
+
+    #convert review counts and votes into float
+    data[["review_count","votes"]]=data[["review_count","votes"]].apply(pd.to_numeric, errors="coerce")
+    return data
