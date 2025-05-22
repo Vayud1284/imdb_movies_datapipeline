2e85d8dc8e612fec3d16be83f01f863f82009059
Vayud1284 <pvayud@gmail.com>
2025-05-22 21:30:44 +0530

    Addint the file

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from src.ingest import load_csv,to_process
+from src.preprocess import process_data
 import os
+import pandas as pd
 
 raw_path=os.path.join("data","raw","imdb-movies-dataset.csv")
 process_path=os.path.join("data","processed","imdb-movies-dataset.parquet")
 df=load_csv(raw_path)
+df=process_data(df)
 to_process(df,process_path)
+
+
+
+
+
