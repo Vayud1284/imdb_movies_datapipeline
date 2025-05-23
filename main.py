06e31952456f94ee6e9b65f323572abf810c4435
Vayud1284 <pvayud@gmail.com>
2025-05-24 01:14:00 +0530

    Updating Main

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ import pandas as pd
 
 raw_path=os.path.join("data","raw","imdb-movies-dataset.csv")
 process_path=os.path.join("data","processed","imdb-movies-dataset.parquet")
-df=load_csv(raw_path)
+'''df=load_csv(raw_path)
 df=process_data(df)
-to_process(df,process_path)
+to_process(df,process_path)'''
 
 
 
 
 
-'''d=pd.read_csv(raw_path)
+d=pd.read_csv(raw_path)
 d.drop_duplicates(inplace=True)
 d.columns=[col.replace("(min)",'') for col in d.columns]
 d.columns=d.columns.str.strip().str.lower().str.replace(" ","_")
-print(d.dtypes)'''
+d.fillna({"certifacate":"not known","genre":"not known","director":"Not known"},inplace=True)
+d["year"]=d["year"].ffill()
+d["duration"]=d["duration"].fillna(d["duration"].mean())
+d["main_genre"]=d["genre"].apply(lambda x:x.split(",")[0])
+print(d.dtypes)
+print(d["main_genre"].tail(10))
